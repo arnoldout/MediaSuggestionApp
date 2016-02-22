f2a84074f1a24515c8759af97b5795b15f72227c
arnoldout <arnoldout@gmail.com>
2016-02-22 20:47:47 +0000

    First Commit
    Created basic form app, which parses JSON into c# objects

---
+++ EntertainmentSuggestionApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntertainmentSuggestionApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dfdg();
+        }
+        public void dfdg()
+        {
+            string baseURI = "http://api.themoviedb.org/3";
+            String srch = "/search/movie?";
+            string apiKey = "api_key=c2dcd458445148b91ed151b2a41a3c22";
+
+            string query = "query=The GodFather&";
+            //converts query into usable string
+            String s = Uri.EscapeUriString(query);
+
+            String uri = baseURI + srch + query + apiKey;
+            //string b = "http://api.themoviedb.org/3/movie/238?api_key=c2dcd458445148b91ed151b2a41a3c22";
+            WebRequest wrGETURL = WebRequest.Create(uri);
+            Stream objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+            dynamic movie = JsonConvert.DeserializeObject(objReader.ReadToEnd());
+            List<Movie> movies = new List<Movie>();
+            foreach (dynamic results in movie.results)
+            { 
+                movies.Add(new Movie(Convert.ToInt32(results.id), Convert.ToString(results.backdrop_path), Convert.ToString(results.title), Convert.ToString(results.overview), Convert.ToString(results.release_date), Convert.ToString(results.vote_average)));
+            }
+        }
+    }
+}
---
+++ EntertainmentSuggestionApp/Movie.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntertainmentSuggestionApp
+{
+    class Movie
+    {
+        int id;
+        String backdropPath;
+        String title;
+        String overview;
+        String releaseDate;
+        String voteAvg;
+
+        public Movie(int id, string backdropPath, string title, string overview, string releaseDate, string voteAvg)
+        {
+            this.id = id;
+            this.backdropPath = backdropPath;
+            this.title = title;
+            this.overview = overview;
+            this.releaseDate = releaseDate;
+            this.voteAvg = voteAvg;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string BackdropPath
+        {
+            get
+            {
+                return backdropPath;
+            }
+
+            set
+            {
+                backdropPath = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Overview
+        {
+            get
+            {
+                return overview;
+            }
+
+            set
+            {
+                overview = value;
+            }
+        }
+
+        public string ReleaseDate
+        {
+            get
+            {
+                return releaseDate;
+            }
+
+            set
+            {
+                releaseDate = value;
+            }
+        }
+
+        public string VoteAvg
+        {
+            get
+            {
+                return voteAvg;
+            }
+
+            set
+            {
+                voteAvg = value;
+            }
+        }      
+    }
+}
